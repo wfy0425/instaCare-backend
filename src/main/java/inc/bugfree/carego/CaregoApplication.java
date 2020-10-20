c2cb3d78980e3f61e1fb478eb726eaf4c59f3b21
Fengyuan Wu <wfy0425@yeah.net>
2020-10-20 09:59:11 +0800

    Initial commit

---
+++ src/main/java/inc/bugfree/carego/CaregoApplication.java
@@ -0,0 +1,13 @@
+package inc.bugfree.carego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaregoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaregoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inc/bugfree/carego/CaregoApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.bugfree.carego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaregoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
