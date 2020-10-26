70e00e795b2bfb378093a9973719322d202417e0
Fengyuan Wu <wfy0425@yeah.net>
2020-10-26 12:29:41 +0800

    update UserBean

--- src/main/java/inc/bugfree/instacare/bean/UserBean.java
+++ src/main/java/inc/bugfree/instacare/bean/UserBean.java
@@ -1,21 +1,27 @@
 package inc.bugfree.instacare.bean;
 
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
 public class UserBean {
 
     private Integer id;
+    private String fullName;
+    private Date createdAt;
+    private Integer userType;
+    private String email;
+    private String password;
+    private String avater;
+    private Integer rating;
+    private List<Integer> requestList;
+    private String description;
+    private String phone;
+    private List<String> addressList;
+    private Integer accumulativeRating;
+    private Integer numOfRating;
+    private String blacklisted;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "UserBean{" +
-                "id=" + id +
-                '}';
-    }
 }
