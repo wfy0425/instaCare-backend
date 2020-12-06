8fc56899e36568115a607d7c21ab0da997f65d19
Fengyuan Wu <wfy0425@yeah.net>
2020-12-06 21:34:50 +0800

    Revert "able to package frontend and backend"
    This reverts commit 2c3c3b530a91a39da5e45a131157ded2080042bb.

--- src/main/java/inc/bugfree/instacare/controller/CommentsController.java
+++ src/main/java/inc/bugfree/instacare/controller/CommentsController.java
@@ -7,7 +7,6 @@ import inc.bugfree.instacare.service.CommentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/comments")
 public class CommentsController {
