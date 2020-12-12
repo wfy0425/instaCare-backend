ba5798b495df22469fa507ee0afc211d1a689b2e
Tianpeng Fan <tif005@ucsd.edu>
2020-12-11 18:25:26 -0800

    add WebApplicationConfig to handle 404 of react router

--- src/main/java/inc/bugfree/instacare/InstacareApplication.java
+++ src/main/java/inc/bugfree/instacare/InstacareApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class InstacareApplication {
 
     public static void main(String[] args) {
